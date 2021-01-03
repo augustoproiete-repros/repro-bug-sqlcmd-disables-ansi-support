447675651a2df9fd8c40a6adb49f154f5c5cd258
C. Augusto Proiete <augusto@proiete.com>
2021-01-03 19:46:17 -0400

    Add repro project for SQLCMD bug disabling ANSI support

---
+++ src/ReproBugSqlCmdDisablesAnsiSupport/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ReproBugSqlCmdDisablesAnsiSupport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // GOOD: This shows in the Console with colors
+            Console.WriteLine("\u001b[41;1m\u001b[37;1mC. Augusto Proiete\u001b[0m");
+
+            Process.Start("sqlcmd", "-???")?.WaitForExit();
+
+            // BAD: ANSI support was disabled by SQLCMD and ANSI colors no longer work
+            Console.WriteLine("\u001b[41;1m\u001b[37;1mC. Augusto Proiete\u001b[0m");
+        }
+    }
+}
